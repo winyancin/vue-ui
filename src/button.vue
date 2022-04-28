e0a42ef6e401462737deaadb1879a3084522f3a2
wuhao <wuhao8882@gmail.com>
2022-04-28 15:47:49 +0800

    Button: 增加 iconfont 并调整相关属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,27 @@
 <template>
-  <button class="d-button">按钮</button>
+  <button class="d-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+export default {
+  props: {
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validate(value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -15,6 +33,10 @@
   border-radius: 4px;
   height: 32px;
   color: #666666;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:focus {
     outline: none;
   }
@@ -22,5 +44,26 @@
     background: var(--button-hover-bg);
     border: 1px solid var(--button-border-hover-color);
   }
+  >.icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  >.content {
+    order: 2;
+  }
+  &.icon-right {
+    >.icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+    >.content {
+      order: 1;
+    }
+  }
+}
+.icon {
+  width: 1em;
+  height: 1em;
 }
 </style>
