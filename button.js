0ba0acd5aae9848a0f135fc28bf1a08da949cffe
wuhao <wuhao8882@gmail.com>
2022-04-28 13:49:45 +0800

    增加 parcel 打包 改写button成单文件组件

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('d-button', {
-    template: `
-        <button class="d-button">按钮</button>
-    `
-})
---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="d-button">按钮</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+.d-button {
+  font-size: var(--button-font-size);
+  padding: 0 1em;
+  background: var(--button-bg);
+  border: 1px solid var(--button-border-color);
+  border-radius: 4px;
+  height: 32px;
+  color: #666666;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background: var(--button-hover-bg);
+    border: 1px solid var(--button-border-hover-color);
+  }
+}
+</style>
