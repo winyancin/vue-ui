4dd6f02f9a0e873336b5539cd6403970c3a10428
wuhao <wuhao8882@gmail.com>
2022-04-28 17:38:22 +0800

    添加 button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button';
+import ButtonGroup from './button-group';
 import Icon from './icon'
 
 Vue.component('d-button', Button)
 Vue.component('d-icon', Icon)
+Vue.component('d-button-group', ButtonGroup)
 
 new Vue({
     el: "#app",
---
+++ src/button-group.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="d-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "button-group",
+  mounted() {
+    console.log(this.$el);
+    for (const child of this.$el.children) {
+      const nodeName = child.nodeName.toLowerCase()
+      if (nodeName !== 'button') {
+        console.warn(`d-button-group 的子元素应该是 d-button, 而不是 ${nodeName}`);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.d-button-group {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  >.d-button {
+    border-radius: 0;
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+    &:first-child {
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+      margin-right: -1px;
+
+    }
+    &:last-child {
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+      margin-left: -1px;
+    }
+  }
+}
+</style>
