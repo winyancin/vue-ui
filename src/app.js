9a6c2637b59cff19d7f4f78ae84ea63ff3ce32e8
wuhao <wuhao8882@gmail.com>
2022-06-13 14:04:05 +0800

    toast 组件: closeButton

--- src/app.js
+++ src/app.js
@@ -1,48 +1,55 @@
-import Vue from 'vue';
-import Button from './button';
-import ButtonGroup from './button-group';
-import Icon from './icon';
-import Input from './input';
-import Row from './row';
-import Col from './col';
-import Layout from './layout';
-import Footer from './footer';
-import Header from './header';
-import Sider from './sider';
-import Content from './content';
-import Toast from './toast';
-import toastPlugin from './plugin';
+import Vue from "vue";
+import Button from "./button";
+import ButtonGroup from "./button-group";
+import Icon from "./icon";
+import Input from "./input";
+import Row from "./row";
+import Col from "./col";
+import Layout from "./layout";
+import Footer from "./footer";
+import Header from "./header";
+import Sider from "./sider";
+import Content from "./content";
+import Toast from "./toast";
+import toastPlugin from "./plugin";
 
-Vue.component('d-button', Button)
-Vue.component('d-icon', Icon)
-Vue.component('d-button-group', ButtonGroup)
-Vue.component('d-input', Input);
-Vue.component('d-row', Row);
-Vue.component('d-col', Col);
-Vue.component('d-layout', Layout);
-Vue.component('d-footer', Footer);
-Vue.component('d-header', Header);
-Vue.component('d-sider', Sider);
-Vue.component('d-content', Content);
-Vue.component('d-toast', Toast);
+Vue.component("d-button", Button);
+Vue.component("d-icon", Icon);
+Vue.component("d-button-group", ButtonGroup);
+Vue.component("d-input", Input);
+Vue.component("d-row", Row);
+Vue.component("d-col", Col);
+Vue.component("d-layout", Layout);
+Vue.component("d-footer", Footer);
+Vue.component("d-header", Header);
+Vue.component("d-sider", Sider);
+Vue.component("d-content", Content);
+Vue.component("d-toast", Toast);
 Vue.use(toastPlugin);
 
 new Vue({
-    el: "#app",
-    data: {
-        loading1: false,
-        loading2: false
+  el: "#app",
+  data: {
+    loading1: false,
+    loading2: false,
+  },
+  // created() {
+  //     this.$toast()
+  // },
+  methods: {
+    inputChange($event) {
+      console.log($event);
     },
-    // created() {
-    //     this.$toast()
-    // },
-    methods: {
-        inputChange($event) {
-            console.log($event);
+
+    showToast() {
+      this.$toast("我是message", {
+        closeButton: {
+          text: "好",
+          cb: () => {
+            console.log("closeTosssxxxast");
+          },
         },
-       
-        showToast() {
-            this.$toast('我是message');
-        }
-    }
-})
+      });
+    },
+  },
+});
--- src/toast.vue
+++ src/toast.vue
@@ -1,19 +1,61 @@
 <template>
-<div class="toast">
-  <slot></slot>
-</div>
+  <div class="toast">
+    <slot></slot>
+    <div class="line" v-if="this.closeButton"></div>
+    <div class="close" @click="onClose" v-if="this.closeButton">{{ this.closeButton.text }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'd-toast'
-}
+  name: "d-toast",
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 3,
+    },
+    closeButton:{
+      type: Object,
+      // default: () => {
+      //   return {
+      //     text: '关闭',
+      //     cb: () => {
+      //       console.log('on close click');
+      //     }
+      //   }
+      // }
+    }
+  },
+  mounted() {
+    if (this.autoClose) {
+      setTimeout(() => {
+        this.close();
+      }, this.autoCloseDelay * 1000);
+    }
+  },
+  methods: {
+    close() {
+      this.$el.remove();
+      this.$destroy();
+    },
+    onClose() {
+      this.close();
+      if (typeof this.closeButton.cb === 'function') {
+        this.closeButton.cb();
+      }
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 $font-size: 14px;
 $toast-height: 40px;
-$toast-bg: rgba(0,0,0,0.75);
+$toast-bg: rgba(0, 0, 0, 0.75);
 .toast {
   display: flex;
   align-items: center;
@@ -28,5 +70,14 @@ $toast-bg: rgba(0,0,0,0.75);
   color: white;
   padding: 0 16px;
   border-radius: 4px;
+}
+.line {
+  border-left: 1px solid #666;
+  height: 100%;
+  margin-left: 16px;
+}
+.close {
+  margin-left: 16px;
+  cursor: pointer;
 }
 </style>
