d84cec99026288ba2f66c81f8c0992713ee1679a
wuhao <wuhao8882@gmail.com>
2022-06-23 13:15:11 +0800

    新增组件文档docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
+module.exports = {
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第dadasdas一个 VuePress 站点",
+  theme: defaultTheme({
+    // 侧边栏数组
+    // 所有页面会使用相同的侧边栏
+    sidebar: [
+      // SidebarItem
+      {
+        text: "入门",
+        // link: "/install/README.md",
+        children: [
+          // SidebarItem
+          {
+            text: "安装",
+            link: "/install/README.md",
+            // children: [],
+          },
+          // 字符串 - 页面文件路径
+          {
+            text: "快速上手",
+            link: "/get-started/README.md",
+            // children: [],
+          },
+        ],
+      },
+      // 字符串 - 页面文件路径
+      {
+        text: "组件",
+        // link: "/install/README.md",
+        children: [
+          // SidebarItem
+          {
+            text: "按钮",
+            link: "/install/README.md",
+            // children: [],
+          },
+          // 字符串 - 页面文件路径
+          {
+            text: "输入框",
+            link: "/get-started/README.md",
+            // children: [],
+          },
+        ],
+      },
+    ],
+  }),
+};
