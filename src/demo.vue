e63c36b47d37ccf9d8050e834ea3c46f609af0db
wuhao <wuhao8882@gmail.com>
2022-06-28 16:42:49 +0800

    新增组件：Pager

--- src/demo.vue
+++ src/demo.vue
@@ -1,44 +1,32 @@
 <template>
   <div>
-    1231231dasdas
-    <!-- <g-nav :selected.sync="selected">
-      <g-nav-item name="home">首页</g-nav-item>
-      <g-sub-nav name="about">
-        <template slot="title">关于</template>
-        <g-nav-item name="culture">企业文化</g-nav-item>
-        <g-nav-item name="developers">开发团队</g-nav-item>
-        <g-sub-nav name="contacts">
-          <template slot="title">联系方式</template>
-          <g-nav-item name="wechat">微信</g-nav-item>
-          <g-nav-item name="qq">QQ</g-nav-item>
-          <g-sub-nav name="phone">
-            <template slot="title">手机</template>
-            <g-nav-item name="cm">移动</g-nav-item>
-            <g-nav-item name="cu">联通</g-nav-item>
-            <g-nav-item name="cn">电信</g-nav-item>
-          </g-sub-nav>
-        </g-sub-nav>
-
-      </g-sub-nav>
-      <g-nav-item name="hire">招聘</g-nav-item>
-    </g-nav>
-    <p>你好，我是中文</p> -->
+    <DPager v-model:current-page="currentPage" :total-pages="10" />
   </div>
 </template>
+
 <script>
-  // import GNav from './nav/nav.vue'
-  // import GNavItem from './nav/nav-item.vue'
-  // import GSubNav from './nav/sub-nav.vue'
-  export default {
-    name: "demo-1",
-    // components: {GNav, GNavItem, GSubNav},
-    // data () {
-    //   return {
-    //     selected: ['culture']
-    //   };
-    // },
-  };
+import DPager from "./pager.vue";
+export default {
+  name: "demo-app",
+  components: { DPager },
+  data() {
+    return {
+      currentPage: 1
+    }
+  }
+};
 </script>
 <style>
-  * {margin: 0; padding: 0; box-sizing: border-box;}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+:root {
+  --button-font-size: 14px;
+  --button-bg: #ffffff;
+  --button-hover-bg: #eeeeee;
+  --button-border-color: #999;
+  --button-border-hover-color: #666666;
+}
 </style>
---
+++ src/pager.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="d-pager">
+    <span class="nav" :class="{ disabled: currentPage === 1 }" @click="onClickPage(currentPage - 1)">
+      <d-icon name="left"></d-icon>
+    </span>
+    <template v-for="page in pages" :key="page">
+      <template v-if="page === currentPage">
+        <span class="d-pager-item active">{{ page }}</span>
+      </template>
+      <template v-else-if="page === '...'">
+        <span class="d-pager-item separator">{{ page }}</span>
+      </template>
+      <template v-else>
+        <span class="d-pager-item" @click="onClickPage(page)">{{ page }}</span>
+      </template>
+    </template>
+    <span class="nav" :class="{ disabled: currentPage === this.totalPages }" @click="onClickPage(currentPage + 1)">
+      <d-icon name="right"></d-icon>
+    </span>
+  </div>
+</template>
+
+<script>
+import DIcon from "./icon.vue";
+export default {
+  name: "d-pager",
+  components: {
+    DIcon,
+  },
+  props: {
+    totalPages: {
+      type: Number,
+      default: 10,
+    },
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    pages() {
+      return unique([
+        1,
+        this.totalPages,
+        this.currentPage,
+        this.currentPage + 1,
+        this.currentPage + 2,
+        this.currentPage - 1,
+        this.currentPage - 2,
+      ])
+        .filter((n) => n > 0 && n <= this.totalPages)
+        .sort((a, b) => a - b)
+        .reduce((prev, current, index, array) => {
+          prev.push(current);
+          if (array[index + 1] - array[index] > 1) {
+            prev.push("...");
+          }
+          return prev;
+        }, []);
+    },
+  },
+  methods: {
+    onClickPage(n) {
+      if (n >= 1 && n <= this.totalPages) {
+        this.$emit("update:currentPage", n);
+      }
+    },
+  },
+};
+
+function unique(array) {
+  return [...new Set(array)];
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/var";
+$width: 20px;
+$height: 20px;
+$font-size: 12px;
+.d-pager {
+  display: flex;
+  &-item {
+    margin: 0 6px;
+    border: 1px solid #e1e1e1;
+    font-size: $font-size;
+    border-radius: 4px;
+    min-width: $width;
+    text-align: center;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    line-height: $height;
+    height: $height;
+    padding: 4px;
+    cursor: pointer;
+    &:hover {
+      border: 1px solid blue;
+    }
+    &.active {
+      border: 1px solid $blue;
+    }
+    &.seperator {
+      border: none;
+    }
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  > .nav {
+    margin: 0 4px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $grey;
+    width: $width;
+    height: $height;
+    border-radius: $border-radius;
+    font-size: $font-size;
+    cursor: pointer;
+    &.disabled {
+      svg {
+        fill: darken($grey, 20%);
+      }
+      cursor:auto;
+    }
+  }
+}
+</style>
